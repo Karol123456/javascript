9970aed1fe17c6a911899e5519bbb793eaec2deb
Karol123456 <kacper.dziebor@tlen.pl>
2021-02-26 13:38:28 +0100

    hfdhdfh
    dfhdfhfdhd

--- main.js
+++ main.js
@@ -1,48 +1,21 @@
-const tablica = new Array();
-function dodawanieDoTablicy(){
-for(let i = 1; i < 100; i++){
-tablica.push(i)
-}
-}
-dodawanieDoTablicy()
-console.log(tablica)
+const arr = new Array(1,15,150,1500,"Jan",5525,"Henryk")
 
-
-
-const wyszukiwanie = tablica.find((item,index) => {
-    if(item === 45){
-        return index
-    }
-    
-})
-console.log(wyszukiwanie)
-
-
-
-const tablicaDruga = new Array(1,15,150,1500)
-
-
-tablicaDruga.forEach(teb =>{
-if(teb === 150) {
-
-}
+arr.forEach(arr =>{
+    console.log(arr)
 })
 
-const tablicaDo1000 = new Array(1,5,6,213,1000)
-const sprawdzenieCzyLiczbaJestRowna1000 = tablicaDo1000.some(item => item === 1000)
-console.log(sprawdzenieCzyLiczbaJestRowna1000)
-
-const tablicaCzwarta = new Array(12,5,8,130,44)
-
-const jdjd = tablicaCzwarta.filter(item => {
-    if(item >=10){
+const sprawdzanie = arr.filter(item => {
+    if(item > 1){
         return item
     }
 })
 
-console.log(jdjd)
+console.log(sprawdzanie)
 
+const indexDlaJana = arr.find((item,index)=>{
+if(item === "Jan"){
+    return index
+}
+})
 
-const tablicaPiata = [5 , 10 ,55 , 130 ,1512]
-const znajdz = tablicaPiata.find(element => element >10);
-console.log(znajdz)
+console.log(indexDlaJana)
